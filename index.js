2fc184a6d9669a7d5e9107de8f7407df1430cb3d
bader mariem <bader.mariem1@gmail.com>
2018-09-21 08:32:06 +0100

    API write

---
+++ index.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const app = express();
+const router = express.Router();
+const port = 3000;
+
+// url: http://localhost:3000/
+app.get('/', (request, response) => response.send('Hello World'));
+
+// all routes prefixed with /api
+app.use('/api', router);
+
+// using router.get() to prefix our path
+// url: http://localhost:3000/api/
+router.get('/', (request, response) => {
+  response.json({message: 'Hello, welcome to my server'});
+});
+
+// set the server to listen on port 3000
+app.listen(port, () => console.log(`Listening on port ${port}`));
+const url = require('url');
+
+router.get('/stuff', (request, response) => {
+  var urlParts = url.parse(request.url, true);
+  var parameters = urlParts.query;
+  var myParam = parameters.myParam;
+  // e.g. myVenues = 12;
+  
+  var myResponse = `I multiplied the number you gave me (${myParam}) by 5 and got: ${myParam * 5}`;
+  
+  response.json({message: myResponse});
+});
+const originWhitelist = ['http://localhost:3000', 'https://example.net'];
+
+// middleware route that all requests pass through
+router.use((request, response, next) => {
+  console.log('Server info: Request received');
+  
+  let origin = request.headers.origin;
+  
+  // only allow requests from origins that we trust
+  if (originWhitelist.indexOf(origin) > -1) {
+    response.setHeader('Access-Control-Allow-Origin', origin);
+  }
+  
+  // only allow get requests, separate methods by comma e.g. 'GET, POST'
+  response.setHeader('Access-Control-Allow-Methods', 'GET');
+  response.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  response.setHeader('Access-Control-Allow-Credentials', true);
+  
+  // push through to the proper route
+  next();
+});
